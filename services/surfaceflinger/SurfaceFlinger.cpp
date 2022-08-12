0d03842f6befb410a40e0fd577decb40967e0770
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-12 23:25:57 +0530

    SurfaceFlinger: Allow forcing HWC brightness support
    Some vendors purposely disable brightness capability
    even though the HWC completely supports it, in order
    to leverage their custom brightness implementations.
    One such example of this is in stock HWC of some
    xiaomi devices.
    
    Since light HAL offers lesser granual control than
    HWC (since its in gamma space and not float), and
    since it would be rather difficult to patch the blob
    to return the right capabilities, allow forcing the
    usage of HWC brightness functions via a prop:
    
    ro.sf.force_hwc_brightness=1 (default 0).
    
    Change-Id: I7d2fef03afbbb936572c717f769f30c9e49213e0
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -450,6 +450,9 @@ SurfaceFlinger::SurfaceFlinger(Factory& factory) : SurfaceFlinger(factory, SkipI
     property_get("ro.sf.force_light_brightness", value, "0");
     mForceLightBrightness = atoi(value);
 
+    property_get("ro.sf.force_hwc_brightness", value, "0");
+    mForceHwcBrightness = atoi(value);
+
     property_get("debug.sf.treat_170m_as_sRGB", value, "0");
     mTreat170mAsSrgb = atoi(value);
 
@@ -1904,12 +1907,8 @@ status_t SurfaceFlinger::getDisplayBrightnessSupport(const sp<IBinder>& displayT
         return NAME_NOT_FOUND;
     }
 
-    if (mForceLightBrightness) {
-        *outSupport = false;
-        return NO_ERROR;
-    }
-
-    *outSupport = getHwComposer().hasDisplayCapability(*displayId, DisplayCapability::BRIGHTNESS);
+    *outSupport = mForceHwcBrightness ? true : mForceLightBrightness ? false :
+            getHwComposer().hasDisplayCapability(*displayId, DisplayCapability::BRIGHTNESS);
     return NO_ERROR;
 }
 
--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -1346,6 +1346,7 @@ private:
 
     bool mLumaSampling = true;
     bool mForceLightBrightness = false;
+    bool mForceHwcBrightness = false;
     sp<RegionSamplingThread> mRegionSamplingThread;
     sp<FpsReporter> mFpsReporter;
     sp<TunnelModeEnabledReporter> mTunnelModeEnabledReporter;
